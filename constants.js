da8762bbc6952ec2ab8b3b8c553d558c639ff105
Aabhas <arinabansal@gmail.com>
2021-06-30 13:16:33 +0530

    completed bot

--- constants.js
+++
@@ -1,3 +0,0 @@
-exports.NAME = 'TTS-bot-AabhasDanger'
-exports.CHANNEL = 'aabhasdanger'
-exports.TOKEN = 'oauth:efnwc93h42dfok4fno604szhytg357'
--- index.js
+++ index.js
@@ -15,16 +15,27 @@ let options = {
 	channels: [ CHANNEL ]
 }
 
+let cooldown = []
+
 const client = new twitch.Client(options);
 
 client.connect().catch(console.error);
 
 client.on('message', (channel, tags, message, self) => {
 	if(self) return;
-	if((tags.mod!=true||tags.badges.admin!='1'||tags.badges.broadcaster!='1')&&message.split(' ')[0]==='!TTS'){
+	if((cooldown.includes(tags.username)!=true||tags.mod==true||tags.badges.broadcaster=='1')&&message.split(' ')[0]==='!TTS'){
         let content = message.split(' ')
         content.shift();
+        if(content.length>20){
+            client.say(channel,`sorry ${tags.username} but the message is too long the limit is 20 words`)
+            return;
+        }
         content = content.join(' ')
         say.speak(content)
+        cooldown.unshift(tags.username)
+        if(tags.mod!=true&&tags.badges.broadcaster!='1'){
+            setTimeout(()=>cooldown.pop(),600000)
+            client.say(channel,`${tags.username} u r now on a cooldown for 10 minutes`)
+        }
     }
 });
